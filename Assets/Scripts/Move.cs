79ab1a4247ecf25acbf01b2d3e3e6048a00d007f
Amhhhex <Amhhex@gmail.com>
2026-01-20 18:38:29 -0500

    Made the ball bounce around the visible screen space

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,63 @@
+
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public Vector2 circlePosition;
+    public float speedX = 0.01f;
+    public float speedY = 0.05f;
+
+    public Vector3 screenPosition;
+
+
+
+
+    public Camera gameCamera;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        circlePosition = transform.position;
+
+        circlePosition.x += speedX;
+        circlePosition.y += speedY;
+
+        transform.position = circlePosition;
+
+        screenPosition = gameCamera.WorldToScreenPoint(transform.position);
+
+        if (screenPosition.x >= Screen.width)
+        {
+            speedX *= -1f;
+        }
+
+        if (screenPosition.x <= 0)
+        {
+
+            speedX *= -1f;
+        }
+
+        if (screenPosition.y >= Screen.height)
+        {
+            speedY *= -1f;
+        }
+
+        if (screenPosition.y <= 0)
+        {
+            speedY *= -1f;
+        }
+
+    }
+}
